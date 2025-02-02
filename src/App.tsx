322377c0d351ef0c8551ba2a3f3b51b8012f0f0e
Alex <puskin1914@gmail.com>
2025-02-02 20:26:44 +0000

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@
 // import viteLogo from '/vite.svg';
 // import './App.css';
 
+import { Button } from './components/ui/button';
+
 function App() {
   // const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
